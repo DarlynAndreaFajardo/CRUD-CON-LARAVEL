5d41775ea5ed8d3cc0f2d858c08a6ccab10e7a7b
Andrea Fajardo <82789231+DarlynAndreaFajardo@users.noreply.github.com>
2021-10-30 00:31:26 -0600

    agregue navbar

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,7 +13,28 @@
     <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="{{ url('/') }}">Usuarios</a>
     </nav>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="Nombre">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ url('/') }}">Email<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/') }}">Fotografia</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/') }}">Rol</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="{{ url('/') }}">Acciones</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <div class="container">
         @yield('contenedor')
     </div>
